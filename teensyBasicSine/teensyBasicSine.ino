e99411fd13e4c07b95a9d56592f9de1c1ba73af0
Andrew Kleindolph <akleindolph@gmail.com>
2015-11-01 21:13:33 -0800

    name fix

--- teensyBasicSine/teensyBasicSine.ino
+++ teensyBasicSine/teensyBasicSine.ino
@@ -9,13 +9,14 @@ AudioSynthWaveformSine   sine1;          //xy=262,298
 AudioOutputI2S           i2s1;           //xy=533,302
 AudioConnection          patchCord1(sine1, 0, i2s1, 0);
 AudioConnection          patchCord2(sine1, 0, i2s1, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=445,227
 // GUItool: end automatically generated code
 
 void setup() {
   AudioMemory(2);   // required to play audio
 
-  audioShield.enable();     //enable audio shield
-  audioShield.volume(0.5);  //set overall volume
+  sgtl5000_1.enable();     //enable audio shield
+  sgtl5000_1.volume(0.5);  //set overall volume
   
   sine1.amplitude(0.5);   // set sine amplitude 
 }
